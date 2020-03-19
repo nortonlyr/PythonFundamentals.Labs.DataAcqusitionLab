77ca699ea03ec5b041c2815a330c9b32715a1279
Norton Li <nli@zipcodes-MBP.home>
2020-03-19 17:11:57 -0400

    make_requests ex done, 39 json files were exported from API

---
+++ make_requests.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+
+file_counter = 0
+offset_counter = 1
+token = 'vgExQgdFRrflyczUiJQVHBXBqzMwqpXz'
+
+while file_counter <= 39:
+    name = ("location_" + str(file_counter))
+    url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/locations?location&limit=1000&offset=' + str(offset_counter)
+    header = {"Token": token, 'Content-Type': 'application/json'}
+    r = requests.get(url=url, headers=header)
+    data = r.json()
+
+    with open('/Users/nli/dev/PythonFundamentals.Labs.DataAcqusitionLab/location_json/' + \
+              (name + ".json"), 'w') as outfile:
+        json.dump(data, outfile)
+    file_counter += 1
+    offset_counter += 1000
+
